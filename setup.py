a927bd4f6960990ab6db10fb427b9b2601149273
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2013-12-18 16:33:35 +1100

    classifiers and licence

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from distutils.core import setup
 
-setup(name='sql',
-      version='0.1.0',
-      description='DB API 2.0 for Humans',
-      long_description=open('README.rst').read(),
-      author='Eugene Van den Bulke',
-      author_email='eugene.vandenbulke@gmail.com',
-      url='http://github.com/3kwa/sql',
-      py_modules=['sql'],
+setup(
+    name='sql',
+    version='0.1.1',
+    description='DB API 2.0 for Humans',
+    long_description=open('README.rst').read(),
+    author='Eugene Van den Bulke',
+    author_email='eugene.vandenbulke@gmail.com',
+    url='http://github.com/3kwa/sql',
+    py_modules=['sql'],
+    license='BSD',
+    classifiers=[
+    'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries'
+        ],
      )
